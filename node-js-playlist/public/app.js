7fdc8a9868644cdca73c80e8b7c60dccf4d6f3c4
donovanmosley <dmosley28@gmail.com>
2020-11-06 18:33:14 -0500

    used express to get index.html, contact, and profile/name page files

--- node-js-playlist/public/app.js
+++ node-js-playlist/public/app.js
@@ -29,16 +29,34 @@
 //     });
 // });
 
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-//  This creates my server:
-var server = http.createServer(function(req, res){
-    console.log('request was made' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+// //  This creates my server:
+// var server = http.createServer(function(req, res){
+//     console.log('request was made' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+//     myReadStream.pipe(res);
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('On port 3000');
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/contact', function(req, res){
+    res.sendFile(__dirname + '/contact.html');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('On port 3000');
+app.get('/profile/:name', function(req, res){
+    res.send('You requested to see a profile with the name of ' + req.params.name);
+});
+
+app.listen(3000);
+
