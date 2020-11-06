8debf620766681ff970fc4b6bdd005964108d3bf
donovanmosley <dmosley28@gmail.com>
2020-11-06 17:32:44 -0500

    added express and nodemon, and installed package.json

---
+++ node-js-playlist/public/scripts.js
@@ -0,0 +1,8 @@
+//Navigation hamburger click function:
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+  if (nav.className != 'active') {nav.className = 'active';}
+  else {nav.className = '';}
+});
