2b9228dd9184664b8b42b8a8be8300da286d5298
donovanmosley <dmosley28@gmail.com>
2020-11-07 06:38:51 -0500

    added partial templates and nav bar functionality using node.js ejs

--- node-js-playlist/public/app.js
+++ node-js-playlist/public/app.js
@@ -47,15 +47,18 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');  
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
 app.get('/profile/:name', function(req, res){
-    res.send('You requested to see a profile with the name of ' + req.params.name);
+    var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
